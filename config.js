dc8cfd6a2a68d973c6b635fb3f327997872856ed
vermin <vermin@techie.com>
2019-11-18 10:44:46 +0100

    :P

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ var api = "http://95.179.152.61:8117";
 // Social Links
 var email = "pool@wazn.io";
 var telegram = "https://t.me/WAZNPool";
-var discord = "https://discordapp.com/invite/WAZNPool";
+var discord = "https://discord.gg/pTDr4hr";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
 // Explorer Links
-var blockchainExplorer = "http://45.32.157.118:8081/{symbol}/block/{id}";
-var transactionExplorer = "http://45.32.157.118:8081/{symbol}/transaction/{id}";
+var blockchainExplorer = "http://explorer.wazn.io/{symbol}/block/{id}";
+var transactionExplorer = "http://explorer.wazn.io/{symbol}/transaction/{id}";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
