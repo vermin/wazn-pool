cf40f5c92cc6f875d0ab0c7e8332a6bceb6d10d4
vermin <vermin@techie.com>
2019-12-14 06:07:40 +0100

    added news, waznrig & https api link

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
 // API Endpoint
-var api = "http://api.wazn.io";
+var api = "https://dig.wazn.io";
 
 // Social Links
+var telegram = "https://t.me/wazn";
 var discord = "https://discord.gg/ZmBra64";
+var facebook = "https://www.facebook.com/wazn.io";
 
 // Market Settings
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
 // Explorer Links
-var blockchainExplorer = "http://explorer.wazn.io/block/{id}";
-var transactionExplorer = "http://explorer.wazn.io/tx/{id}";
+var blockchainExplorer = "https://explorer.wazn.io/block/{id}";
+var transactionExplorer = "https://explorer.wazn.io/tx/{id}";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
